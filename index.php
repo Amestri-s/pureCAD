0bb8042f8378063cb06169c4dc902f976d40b235
Amestri-s <btamestris@gmail.com>
2020-09-06 19:30:12 +0100

    Secondary commit

--- index.php
+++ index.php
@@ -3,4 +3,32 @@
     $pageTitle = "Home";
 
     require "require/head.php";
+    require "require/footer.php";
 ?>
+<div class="p-2 row d-flex justify-content-center">
+  <div class="card p-2 m-1 col-md-2">
+    <h1 class="display-5 ">Get <br><strong class="text-primary">Started</strong></h1><br>
+    <a class="btn blue-gradient" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" selected="false">Sign In</a>
+    <a class="btn blue-gradient" id="signup-tab" data-toggle="tab" href="#signup" role="tab" aria-controls="signup" selected="false">Sign Up</a>
+    <a class="btn btn-outline-primary btn-sm" id="forgot-tab" data-toggle="tab" href="#forgot" role="tab" aria-controls="forgot" selected="false">Forgot Password</a>
+  </div>
+  <div class="card p-2 m-1 col-md-4">
+    <h1 class="display-5 ">Community <br><strong class="text-primary">Info</strong></h1><br>
+    <p class="p-2 blue lighten-5"><strong class="badge blue-gradient">Name</strong><br> <?php echo $optionsArray['communityName']; ?></p>
+    <p class="p-2 blue lighten-5"><strong class="badge blue-gradient">Description</strong><br> <?php echo $optionsArray['communityDesc']; ?></p>
+    <p class="p-2 blue lighten-5"><strong class="badge blue-gradient">Members</strong><br> <?php echo $noOfUsers; ?></p>
+  </div>
+</div>
+<div class="p-2 row d-flex justify-content-center">
+    <div class="tab-content" id="indexTabContent">
+        <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+            <p>Login</p>
+        </div>
+        <div class="tab-pane fade" id="signup" role="tabpanel" aria-labelledby="signup-tab">
+            <p>Signup</p>
+        </div>
+        <div class="tab-pane fade" id="forgot" role="tabpanel" aria-labelledby="forgot-tab">
+            <p>Forgot</p>
+        </div>
+    </div>
+</div>
--- require/head.php
+++ require/head.php
@@ -1,10 +1,17 @@
+<html lang="en">
+
+
 <?php
     //Creating connection for the variable $link
-    require "config.php";
+    require "mysqlConnection.php";
 
     //Query for grabbing configuration options
-    $optionsQuery = mysqli_query($link, "SELECT * FROM config")
+    $optionsQuery = mysqli_query($link, "SELECT * FROM config");
     $optionsArray = mysqli_fetch_array($optionsQuery);
+
+    //Query for grabbing number of users
+    $noOfUsersQuery = mysqli_query($link, "SELECT * FROM users");
+    $noOfUsers = mysqli_num_rows($noOfUsersQuery);
 ?>
 
 <head>
@@ -20,4 +27,50 @@
   <link href="css/mdb.min.css" rel="stylesheet">
   <!-- Your custom styles (optional) -->
   <link href="css/style.css" rel="stylesheet">
+
+  <meta property="og:title" content="<?php echo $optionsArray['communityName']; ?>">
+  <meta property="og:site_name" content="<?php echo $optionsArray['communityName']; ?>">
+  <meta property="og:url" content="https://scfr.site/scfr">
+  <meta property="og:description" content="The aim of Pure CAD is to provide an intuitive CAD that users enjoy.">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="<?php echo $optionsArray['communityLogo']; ?>">
 </head>
+
+<body>
+    <!--Navbar -->
+    <nav class="mb-1 navbar navbar-expand-lg navbar-dark blue-gradient">
+      <a class="navbar-brand" href="#"><?php echo $optionsArray['communityName']; ?></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+        aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CAD</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Support</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto nav-flex-icons">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-user"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-top dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!--/.Navbar -->
