836cb74f6de1ade7d5200b2b4e28dd6a82261b8a
Amestri-s <btamestris@gmail.com>
2020-09-29 19:56:57 +0100

    Echo any errors

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
             if(mysqli_stmt_execute($stmt)){
               mysqli_stmt_store_result($stmt);
               if(mysqli_stmt_num_rows($stmt) == 1){
-                mysqli_stmt_bind_result($stmt, $id, $username, $hashedPassword, $selectedCommunity);
+                mysqli_stmt_bind_result($stmt, $id, $user, $hashedPassword, $selectedCommunity);
                 if(password_verify($password, $hashedPassword)){
                   session_start();
                   $_SESSION['userID'] == $id;
